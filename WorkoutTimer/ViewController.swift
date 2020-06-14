fb6306c0b8548b5f2f19bcb3961104d14e71af63
kynmh69 <hiroki.kayanuma@gmail.com>
2020-06-14 17:58:47 +0900

    first release

--- WorkoutTimer/ViewController.swift
+++ WorkoutTimer/ViewController.swift
@@ -19,25 +19,66 @@ class ViewController: UIViewController {
     @objc var timer: Timer?
     var counter = 0
     var counterSec = 0
-    var timerInfo = TimerInfo(interval: 1, setCount: 10, second: 5)
+    var timerInfo = TimerInfo(interval: 5, setCount: 10, second: 0)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // set default.
+        self.timeLbl.text = "\(self.timerInfo.setCount).\(self.timerInfo.second)"
     }
 
     @IBOutlet weak var timeLbl: UILabel!
     
+    @IBOutlet weak var countField: UITextField!
+    
+    @IBOutlet weak var intervalField: UITextField!
+    
+    
+    /// if tap start button
+    /// - Parameter sender: <#sender description#>
     @IBAction func startBt(_ sender: Any) {
+        self.getFields()
         self.timeLbl.text = "\(self.timerInfo.setCount).\(self.timerInfo.second)"
         print("timer: \(self.timerInfo.setCount).\(self.timerInfo.second)")
         self.counter = self.timerInfo.setCount
         self.counterSec = self.timerInfo.second
-        self.timer = Timer.scheduledTimer(timeInterval: TimeInterval(self.timerInfo.interval), target: self, selector: #selector(ViewController.timerUpdate), userInfo: nil, repeats: true)
+        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerUpdate), userInfo: nil, repeats: true)
     }
+    
+    
+    /// if tap stop button
+    /// - Parameter sender: <#sender description#>
     @IBAction func stopBt(_ sender: Any) {
         self.timer?.invalidate()
     }
     
+    
+    /// if tap reset button
+    /// - Parameter sender: <#sender description#>
+    @IBAction func resetBt(_ sender: Any) {
+        self.resetTimer()
+    }
+    
+    
+    /// フィールドから取得する
+    fileprivate func getFields() {
+        self.timerInfo.setCount = Int(self.countField.text!) ?? self.timerInfo.setCount
+        print("timerInfo.setCount\(self.timerInfo.setCount)")
+        self.timerInfo.interval = Int(self.intervalField.text!) ?? self.timerInfo.interval
+        print("timerInfo.second: \(self.timerInfo.second)")
+    }
+    
+    /// キーボード以外のところをタップしたら、フィールドの値を取得して閉じる
+    /// - Parameters:
+    ///   - touches: <#touches description#>
+    ///   - event: <#event description#>
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.getFields()
+
+        self.view.endEditing(true)
+    }
+    
+    
+    /// タイマーをカウントダウンする
     @objc func timerUpdate() {
         self.counterSec -= 1
         if self.counter == 0 && self.counterSec == 0 {
@@ -45,7 +86,7 @@ class ViewController: UIViewController {
             self.resetTimer()
         }
         if self.counterSec < 0 {
-            self.counterSec = self.timerInfo.second
+            self.counterSec = self.timerInfo.interval
             self.counter -= 1
         }
         
@@ -53,7 +94,10 @@ class ViewController: UIViewController {
         self.timeLbl.text = "\(self.counter).\(self.counterSec)"
     }
     
+    /// タイマーをリセットする
     func resetTimer() {
+        self.timerInfo.setCount = 10
+        self.timerInfo.second = 0
         self.counter = self.timerInfo.setCount
         self.counterSec = self.timerInfo.second
         self.timeLbl.text = "\(self.counter).\(self.counterSec)"
