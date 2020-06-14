aa30ad35bfb94022505b88908d2599cbf1c69e98
kynmh69 <hiroki.kayanuma@gmail.com>
2020-06-14 16:27:25 +0900

    create Timer

--- WorkoutTimer/ViewController.swift
+++ WorkoutTimer/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct TimerInfo {
+    var interval: Int
+    var setCount: Int
+    var second: Int
+}
 
+
+class ViewController: UIViewController {
+    @objc var timer: Timer?
+    var counter = 0
+    var counterSec = 0
+    var timerInfo = TimerInfo(interval: 1, setCount: 10, second: 5)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var timeLbl: UILabel!
+    
+    @IBAction func startBt(_ sender: Any) {
+        self.timeLbl.text = "\(self.timerInfo.setCount).\(self.timerInfo.second)"
+        print("timer: \(self.timerInfo.setCount).\(self.timerInfo.second)")
+        self.counter = self.timerInfo.setCount
+        self.counterSec = self.timerInfo.second
+        self.timer = Timer.scheduledTimer(timeInterval: TimeInterval(self.timerInfo.interval), target: self, selector: #selector(ViewController.timerUpdate), userInfo: nil, repeats: true)
+    }
+    @IBAction func stopBt(_ sender: Any) {
+        self.timer?.invalidate()
+    }
+    
+    @objc func timerUpdate() {
+        self.counterSec -= 1
+        if self.counter == 0 && self.counterSec == 0 {
+            self.timer?.invalidate()
+            self.resetTimer()
+        }
+        if self.counterSec < 0 {
+            self.counterSec = self.timerInfo.second
+            self.counter -= 1
+        }
+        
+        print("timer: \(self.counter).\(self.counterSec)")
+        self.timeLbl.text = "\(self.counter).\(self.counterSec)"
+    }
+    
+    func resetTimer() {
+        self.counter = self.timerInfo.setCount
+        self.counterSec = self.timerInfo.second
+        self.timeLbl.text = "\(self.counter).\(self.counterSec)"
+    }
 }
 
