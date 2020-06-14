e6f1c4778fda06e6eefdcfabac434596b2ca00ee
kynmh69 <hiroki.kayanuma@gmail.com>
2020-06-14 15:19:07 +0900

    first commit

---
+++ WorkoutTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutTimer
+//
+//  Created by Hiroki Kayanuma on 2020/06/14.
+//  Copyright © 2020 Hiroki Kayanuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
